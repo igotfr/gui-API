61ada12bff26c36f30e398d86e6ef321ddf72a74
Igor Ferreira <32280512+igotfr@users.noreply.github.com>
2023-06-20 20:19:55 -0300

    Update button.rs

--- button/button.rs
+++ button/button.rs
@@ -1,11 +1,18 @@
-// like iced https://docs.rs/iced/latest/iced/
-button(content: impl Into<Element<'a, Message, Renderer>>) -> widget::Button<'a, Message, Renderer>
+// https://docs.rs/iced/latest/iced/
+// like iced https://docs.rs/iced_native/latest/iced_native/widget/button/struct.Button.html
+button<'a, Message, Renderer>(content: impl Into<Element<'a, Message, Renderer>>) -> widget::Button<'a, Message, Renderer>
+where
+    Renderer: crate::Renderer,
+    Renderer::Theme: widget::button::StyleSheet,
+    <Renderer::Theme as widget::button::StyleSheet>::Style: Default,
+
   .width(width: impl Into<Length>)
   .height(height: impl Into<Length>)
   .padding(padding: impl Into<Padding>)
-  .style()
+  .style(style: <Renderer::Theme as StyleSheet>::Style)
 
-  .on_press() // Observer Pattern
+  .on_press(msg: Message) // Observer Pattern
 
+  // core Rust
   .into()
   .try_into()
