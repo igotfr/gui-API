1dc35a3298a9aee498c19c34fa90f736b5d9ab9a
Igor Ferreira <32280512+igotfr@users.noreply.github.com>
2023-08-07 11:07:07 -0300

    Create font_weight.rs

---
+++ text/font_weight.rs
@@ -0,0 +1,22 @@
+tuple FontWeight(u32);
+
+impl FontWeight {
+  const W100: FontWeight = FontWeight(100);
+  const W200: FontWeight = FontWeight(200);
+  const W300: FontWeight = FontWeight(300);
+  const W400: FontWeight = FontWeight(400);
+  const W500: FontWeight = FontWeight(500);
+  const W600: FontWeight = FontWeight(600);
+  const W700: FontWeight = FontWeight(700);
+  const W800: FontWeight = FontWeight(800);
+  const W900: FontWeight = FontWeight(900);
+  const Thin: FontWeight = FontWeight(100);
+  const ExtraLight: FontWeight = FontWeight(200);
+  const Light: FontWeight = FontWeight(300);
+  const Normal: FontWeight = FontWeight(400);
+  const Medium: FontWeight = FontWeight(500);
+  const SemiBold: FontWeight = FontWeight(600);
+  const Bold: FontWeight = FontWeight(700);
+  const ExtraBold: FontWeight = FontWeight(800);
+  const Black: FontWeight = FontWeight(900);
+}
