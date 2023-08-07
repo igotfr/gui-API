36bea4cd6659adc686d842f9eac3e3b53782d013
Igor Ferreira <32280512+igotfr@users.noreply.github.com>
2023-08-07 11:09:12 -0300

    Update text.rs

--- text/text.rs
+++ text/text.rs
@@ -15,7 +15,7 @@ where
 
   // jetpack terminology
   .font_size(font_size: impl Into<Pixels>)
-  .font_style(font_style: FontStyle)
+  .font_style(font_style: impl Into<FontStyle>)
   .font_weight(font_weight: impl Into<FontWeight>)
   .font_family(font_family: FontFamily)
 
@@ -26,33 +26,3 @@ where
   .clone()
   .clone_into()
   .clone_from()
-
-tuple FontStyle(u32);
-
-impl FontStyle {
-  const Normal: FontStyle = FontStyle(0);
-  const Italic: FontStyle = FontStyle(1);
-}
-
-tuple FontWeight(u32);
-
-impl FontWeight {
-  const W100: FontWeight = FontWeight(100);
-  const W200: FontWeight = FontWeight(200);
-  const W300: FontWeight = FontWeight(300);
-  const W400: FontWeight = FontWeight(400);
-  const W500: FontWeight = FontWeight(500);
-  const W600: FontWeight = FontWeight(600);
-  const W700: FontWeight = FontWeight(700);
-  const W800: FontWeight = FontWeight(800);
-  const W900: FontWeight = FontWeight(900);
-  const Thin: FontWeight = FontWeight(100);
-  const ExtraLight: FontWeight = FontWeight(200);
-  const Light: FontWeight = FontWeight(300);
-  const Normal: FontWeight = FontWeight(400);
-  const Medium: FontWeight = FontWeight(500);
-  const SemiBold: FontWeight = FontWeight(600);
-  const Bold: FontWeight = FontWeight(700);
-  const ExtraBold: FontWeight = FontWeight(800);
-  const Black: FontWeight = FontWeight(900);
-}
